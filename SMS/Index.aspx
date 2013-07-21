155fb20c53be0ad3edfcfa30afe568b553f995cd
Ubaid Khan <ubaidkhan88@gmail.com>
2013-07-21 13:24:26 +0300

    added uploading image and update panels

--- SMS/Index.aspx
+++ SMS/Index.aspx
@@ -1,20 +1,73 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="SMS_Index" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Index.aspx.cs" Inherits="SMS_Index" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
-<h3 class="left">
-    Sent SMS
-  </h3>
-<h3 class="right">
-    <a href="New.aspx">New</a> 
-  </h3>
-  <div class="clear"></div>
-  <hr />
-    <asp:GridView ID="gvSMS" runat="server" AllowPaging="True" CssClass="table table-bordered" 
-        Width="100%" EmptyDataText="No Sent SMS!" 
-        onpageindexchanging="gvSMS_PageIndexChanging" 
-        onrowdatabound="gvSMS_RowDataBound" PageSize="30">
-    </asp:GridView>
-</asp:Content>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="Server">
+    <script type="text/javascript">
+        $(function () {
+            Sys.WebForms.PageRequestManager.getInstance().add_endRequest(endRequestHandler);
 
+            function endRequestHandler(sender, args) {
+                $(".sent-date").datepicker({
+                    constrainInput: true,
+                    dateFormat: "dd-mm-yy",
+                    changeMonth: true,
+                    changeYear: true
+                });
+
+                $(".sent-date").attr("readonly", true);
+            }
+        });
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="Server">
+    <asp:ScriptManager ID="ScriptManager" runat="server">
+    </asp:ScriptManager>
+    <asp:UpdatePanel ID="searchUpdatePane" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <fieldset>
+                <legend>Search </legend>
+                <table class="table table-bordered">
+                    <tr>
+                        <td>
+                            <label>
+                                Search Field</label>
+                            <asp:DropDownList ID="ddlSearchField" runat="server" OnSelectedIndexChanged="ddlSearchField_SelectedIndexChanged">
+                            </asp:DropDownList>
+                        </td>
+                        <td>
+                            <label>
+                                Value</label>
+                            <asp:TextBox ID="txtSentDate" runat="server" CssClass="sent-date"></asp:TextBox>
+                            <asp:TextBox ID="txtField" runat="server"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="rfvSentDate" runat="server" ControlToValidate="txtSentDate"
+                                ForeColor="#FF3300" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                            <asp:RequiredFieldValidator ID="rfvField" runat="server" ControlToValidate="txtField"
+                                ForeColor="#FF3300" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                        </td>
+                        <td>
+                            <asp:Button ID="btnSearch" runat="server" Text="Search" CssClass="btn btn-info" 
+                                onclick="btnSearch_Click" />
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+    <asp:UpdatePanel ID="gvUpdatePanel" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <h3 class="left">
+                Sent SMS
+            </h3>
+            <h3 class="right">
+                <a href="New.aspx">New</a>
+            </h3>
+            <div class="clear">
+            </div>
+            <hr />
+            <asp:GridView ID="gvSMS" runat="server" AllowPaging="True" CssClass="table table-bordered"
+                Width="100%" EmptyDataText="No Sent SMS!" OnPageIndexChanging="gvSMS_PageIndexChanging"
+                OnRowDataBound="gvSMS_RowDataBound" PageSize="30">
+            </asp:GridView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
--- SMS/Index.aspx.cs
+++ SMS/Index.aspx.cs
@@ -12,7 +12,12 @@ public partial class SMS_Index : System.Web.UI.Page
     protected SMS_EMAIL_DB_Entities _sms_EMAIL_DB_Entities;
     protected void Page_Load(object sender, EventArgs e)
     {
-        BindDataToGridView();
+        if (!IsPostBack)
+        {
+            BindDataToGridView();
+            BindSearchFieldDdl();
+            UpdateTextBoxAndRfv(false, false);
+        }
     }
     protected void gvSMS_PageIndexChanging(object sender, GridViewPageEventArgs e)
     {
@@ -24,6 +29,7 @@ public partial class SMS_Index : System.Web.UI.Page
         if (e.Row.RowType != DataControlRowType.DataRow) return;
         LinkButton lb;
         lb = new LinkButton();
+        lb.CausesValidation = false;
         lb.CommandArgument = e.Row.Cells[4].Text;
         lb.CommandName = "NumClick";
         lb.Text = "Details";
@@ -43,16 +49,36 @@ public partial class SMS_Index : System.Web.UI.Page
         dt.Columns.Add(new DataColumn("Status", typeof(string)));
         dt.Columns.Add(new DataColumn("Details", typeof(string)));
 
+        IQueryable<tbl_Emails_SMS> ls;
         List<tbl_Emails_SMS> list;
         if (CurrentUser.Role() == "Admin")
         {
-            list = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.Type == "SMS").OrderByDescending(x => x.Created_At).ToList();
+            ls = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.Type == "SMS");
         }
         else
         {
             var currentUserId = CurrentUser.Id();
-            list = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.Type == "SMS").Where(x => x.User_Id == currentUserId).OrderByDescending(x => x.Created_At).ToList();
+            ls = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.Type == "SMS").Where(x => x.User_Id == currentUserId);
         }
+
+        switch (ddlSearchField.SelectedValue)
+        {
+            case "Mobile Number":
+                ls = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.Mobile_Number.Contains(txtField.Text));
+                break;
+            case "Date":
+                var date = txtSentDate.Text;
+                var startDate = DateTime.Parse(date + " 00:00:00");
+                var endDate = DateTime.Parse(date + " 23:59:59");
+                ls = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.SMS_Sent_At >= startDate).Where(x => x.SMS_Sent_At <= endDate);
+                break;
+            case "Sent By":
+                ls = _sms_EMAIL_DB_Entities.tbl_Emails_SMS.Where(x => x.tbl_Users.User_Name.Contains(txtField.Text));
+                break;
+            default: break;
+        }
+        list = ls.OrderByDescending(x => x.Created_At).ToList();
+
         foreach (var x in list)
         {
             dr = dt.NewRow();
@@ -65,5 +91,66 @@ public partial class SMS_Index : System.Web.UI.Page
         }
         gvSMS.DataSource = dt;
         gvSMS.DataBind();
+    }
+
+    protected void BindSearchFieldDdl()
+    {
+        _sms_EMAIL_DB_Entities = new SMS_EMAIL_DB_Entities();
+        DataTable table = new DataTable();
+        table.Columns.Add("Text");
+        table.Columns.Add("Value");
+        DataRow dr;
+        List<string> lst = new List<string> { "None / All", "Mobile Number", "Date" };
+        if (CurrentUser.Role() == "Admin")
+        {
+            lst.Add("Sent By");
+        }
+        foreach (var x in lst)
+        {
+            dr = table.NewRow();
+            dr["Value"] = x;
+            dr["Text"] = x;
+            table.Rows.Add(dr);
+        }
+        ddlSearchField.DataSource = table;
+        ddlSearchField.DataTextField = table.Columns["Text"].ColumnName;
+        ddlSearchField.DataValueField = table.Columns["Value"].ColumnName;
+        ddlSearchField.AutoPostBack = true;
+        ddlSearchField.DataBind();
+        ddlSearchField.SelectedIndexChanged += new System.EventHandler(BindCategories);
+    }
+
+    protected void ddlSearchField_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlSearchField.SelectedValue == "None / All")
+        {
+            UpdateTextBoxAndRfv(false, false);
+        }
+        else if (ddlSearchField.SelectedValue == "Date")
+        {
+            UpdateTextBoxAndRfv(false, true);
+        }
+        else
+        {
+            UpdateTextBoxAndRfv(true, false);
+        }
+    }
+
+    private void BindCategories(object sender, EventArgs e)
+    {
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        BindDataToGridView();
+        gvUpdatePanel.Update();
+    }
+
+    protected void UpdateTextBoxAndRfv(bool first, bool second)
+    {
+        txtField.Visible = first;
+        rfvField.Enabled = first;
+        txtSentDate.Visible = second;
+        rfvSentDate.Enabled = second;
     }
 }
